1d73231ca600c167a6d17e357c47b8ee8e0fe358
sksunil0608 <sksunil0608@gmail.com>
2023-10-29 11:24:26 +0530

    Hook and UseState

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Board from './components/Board';
+
+import './styles.scss';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [state,setState] = useState(1);
+  const onBtnClick = () => {
+    console.log('hello');
+    // setCounter(counter + 1);
+    // send a callback fuction here in place of this
+    setCounter(currentCounter => {
+      return currentCounter + 1;
+    });
+  };
+  const [counter, setCounter] = useState(1);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <Board />
+      <button type="button" onClick={onBtnClick}>
+        click Me
+      </button>
+      <h1>{counter}</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
