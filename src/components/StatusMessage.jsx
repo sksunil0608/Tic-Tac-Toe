e9a138f79e88cafebb08a907dfd7af25f7f3665b
sksunil0608 <sksunil0608@gmail.com>
2023-10-29 17:12:59 +0530

    Status Message and Draw Game

---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+//React.Fragment is the empty tag we can use <> </>
+const StatusMessage = ({ winner, isXNext, squares }) => {
+  const noMovesLeft = squares.every(squareValue => squareValue !== null);
+  const nextPlayer = isXNext ? 'X' : 'O';
+
+  const renderStatusMessage = () => {
+    if (winner) {
+      return (
+        <>
+          {' '}
+          Winner is{' '}
+          <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      );
+    }
+
+    if (!winner && noMovesLeft) {
+      return (
+        <>
+          <span className="text-green">X</span> and{' '}
+          <span className="text-orange">O</span> ties
+        </>
+      );
+    }
+
+    if (!winner && !noMovesLeft) {
+      return (
+        <>
+          Next Player is{' '}
+          <span className={isXNext ? 'text-green' : 'text-orange'}>
+            {nextPlayer}
+          </span>
+        </>
+      );
+    }
+
+    return null;
+  };
+
+  //   return <div>{winner ? <div></div> : null}</div>;
+  return <div className="status-message">{renderStatusMessage()}</div>;
+};
+
+export default StatusMessage;
