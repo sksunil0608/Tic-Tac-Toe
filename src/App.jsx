49a7e0f8ecb43cbdd72c78d1e5ec62a08b3ab040
sksunil0608 <sksunil0608@gmail.com>
2023-10-29 12:50:56 +0530

    Square Click Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import Board from './components/Board';
 
 import './styles.scss';
-import { useState } from 'react';
 
 function App() {
-  // const [state,setState] = useState(1);
-  const onBtnClick = () => {
-    console.log('hello');
-    // setCounter(counter + 1);
-    // send a callback fuction here in place of this
-    setCounter(currentCounter => {
-      return currentCounter + 1;
-    });
-  };
-  const [counter, setCounter] = useState(1);
-
   return (
     <div className="app">
       <Board />
-      <button type="button" onClick={onBtnClick}>
-        click Me
-      </button>
-      <h1>{counter}</h1>
     </div>
   );
 }
